d9cb3ce6f95d7633b8debc0c0be1c505b08b593c
Olga Kr <olichka121kr76@gmail.com>
2018-10-23 09:54:56 +0300

    Add lab8

--- SportProject/assets/js/main.js
+++ SportProject/assets/js/main.js
@@ -48,21 +48,34 @@ function validate(form) {
 	}
 
 	if (valid) {
-		addFeedback(elems.fanname.value, elems.feedback.value);
+		if (online) {
+			addFeedback(elems.fanname.value, elems.feedback.value);
+		}
+		else {
+			addFeedbackToStorage(elems.fanname.value, elems.feedback.value);
+			var amount = localStorage.getItem('feedbacks');
+			var value = JSON.parse(localStorage.getItem('feedbacks' + amount));
+			// addFeedback(value['fanname'], value['feedback'], value['date']);
+		}
 		form.reset();
 	}	
 }
 
-function addElement(parentId, elementTag, html) {
+function addElement(parentId, elementTag, html, position) {
 	var p = document.getElementById(parentId);
 	var newElement = document.createElement(elementTag);
 	newElement.innerHTML = html;
 	p.appendChild(newElement);
 	var div = document.getElementById(parentId);
-	div.insertBefore(newElement, div.childNodes[1]);
+	if (position == 'before') {
+		div.insertBefore(newElement, div.childNodes[1]);
+	}
+	else {
+		div.insertBefore(newElement, div.lastChild.nextSibling);
+	}	
 }
 
-function addFeedback(name, feedback) {
+function addFeedback(name, feedback, date) {
 	var divElem = document.getElementById('feedbacks');
 	var classElem = divElem.childNodes[1].getAttribute("class");
 
@@ -71,14 +84,17 @@ function addFeedback(name, feedback) {
 	} else {
 		classTheme = "light";
 	}
-	var today = currentDate();
+	if (!date) {
+		date = currentDate();
+	}
+	
 	var html = '<div class="feedbacks-' + classTheme + ' col-md-12"><p>' + feedback + '</p>'
 	+'<div class="info">'
-	+'<div class="author-' + classTheme + '">' + today + '</div>'
+	+'<div class="author-' + classTheme + '">' + date + '</div>'
 	+'<div class="author-' + classTheme + '">' + name + '</div>'
 	+'</div>'
 	+'</div>';
-	addElement('feedbacks', 'div', html);
+	addElement('feedbacks', 'div', html, 'before');
 }
 
 function currentDate() {
@@ -124,15 +140,129 @@ function validateNew(form) {
 		valid = false;
 	}
 
-	resetError(elems.feedback.parentNode);
-	if (!elems.feedback.value) {
-		showError(elems.feedback.parentNode, ' Please, enter a text of new.');
+	resetError(elems.news.parentNode);
+	if (!elems.news.value) {
+		showError(elems.news.parentNode, ' Please, enter a text of new.');
 		valid = false;
 	}
 
 	if (valid) {
+		addNewToLocal(elems.nname.value, elems.news.value);
+		document.getElementById('clock').src = "";
 		form.reset();
 		alert("Your new is successfully posted.")
 
 	}	
+}
+
+function isOnline() {
+	return window.navigator.onLine;
+}
+
+function onLoadFeedbacks() {
+	if (isOnline()) {
+		getFeedbacksFromLocal();
+		online = true;
+	}
+	else {
+		online = false;
+	}
+}
+
+function onLoadNews() {
+	if (isOnline()) {
+		getNewsFromLocal();
+		online = true;
+	}
+	else {
+		online = false;
+	}
+}
+
+function addFeedbackToStorage(name, feedback) {
+	var dict = {
+		'fanname': name,
+		'feedback': feedback,
+		'date': currentDate()
+	};
+	var amount = localStorage.getItem('feedbacks');
+	alert(amount);
+	if (!amount) {
+		localStorage.setItem('feedbacks', 0);
+		amount = 0;
+	}
+	amount++;
+	localStorage.setItem('feedbacks' + ("" + amount), JSON.stringify(dict));
+	localStorage.setItem('feedbacks', amount);
+}
+
+function getFeedbacksFromLocal() {
+	var amount = localStorage.getItem('feedbacks');
+	for (var i = 1; i <= amount; i++) {
+		var value = JSON.parse(localStorage.getItem('feedbacks' + ("" + i)));
+		addFeedback(value["fanname"], value["feedback"], value["date"]);
+	}
+}
+
+function fromLocalToServer() {
+	alert('All data is on the server now');
+}
+
+function addNewToLocal(name, news) {
+	img = document.getElementById('clock');
+	imgNew = getImageURL(img);
+	var dict = {
+		'img': imgNew,
+		'newname': name,
+		'newtext': news
+	};
+	var amount = localStorage.getItem('news');
+	if (!amount) {
+		localStorage.setItem('news', 0);
+		amount = 0;
+	}
+	amount++;
+	localStorage.setItem('news' + ("" + amount), JSON.stringify(dict));
+	localStorage.setItem('news', amount);
+}
+
+function getImageURL(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+function getNewsFromLocal() {
+	var amount = localStorage.getItem('news');
+	var id;
+	for (var i = 1; i <= amount; i++) {
+		var value = JSON.parse(localStorage.getItem('news' + i));
+		if (i%4 == 1) {
+			id = 'news' + i;
+			addElement('news', 'div', '<div class="col-md-12" id=' + id +'></div>')
+		}
+		addNew(value["img"], value["newname"], id);
+	}
+}
+
+function addNew(img, name, id) {
+	var divElem = document.getElementById(id);
+	var html = '<div class="col-md-3">' +
+				'<div class="news-content">' +
+				'<div class="cell-content">' +
+				'<img src="data:image/png;base64,' + img + '" alt="">' +
+				'<a href="#"><span id="clickable-area"></span>' +
+				'<h3>' + name + '</h3>' +
+				'</a>' +
+				'</div>' +
+				'</div>' +
+				'</div>';
+	addElement(id, 'div', html, 'after');
 }
